8856929d7d76b79a32bdafef5179ac1f22ff831b
Blemy <95835606+Blemy@users.noreply.github.com>
2023-11-13 16:04:55 -0500

    Update setup.py

--- pdfgen/setup.py
+++ pdfgen/setup.py
@@ -4,7 +4,7 @@ setup(
     name='pdf_gen',
     description='Ce package permet de créer un fichier PDF dynamique',
     version='0.1',
-    packages=['nomdupackage'],
+    packages=['pdf_gen'],
     author='Ing Blemy & Ing Wenso',
     author_email='mathieublemy@2001gmail.com & wensoalseille@gamil.com'
 )
